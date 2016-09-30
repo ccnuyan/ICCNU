95cda679d2e81603e82854014d018a31029cc0db
System Administrator <root@yans-MacBook-Pro.local>
2016-09-30 16:24:10 +0800

    fix a big ie bug

--- dependencies.js
+++ dependencies.js
@@ -7,6 +7,7 @@ var dependencies = {
     ],
     js: [
         '//cdn.bootcss.com/modernizr/2.8.3/modernizr.min.js',
+        '//cdn.bootcss.com/es6-promise/4.0.2/es6-promise.min.js',
         '//cdn.bootcss.com/fetch/1.0.0/fetch.min.js',
         '//cdn.bootcss.com/classlist/2014.01.31/classList.min.js',
         '//cdn.bootcss.com/jquery/1.9.1/jquery.min.js',
